1c3546a8db3648acca90b35c01c4f6a4c1503b32
BenAliasMacc <80431649+BenAliasMacc@users.noreply.github.com>
2023-04-25 15:50:37 +0200

    fix prod bug

--- api/config/allowedOrigins.js
+++ api/config/allowedOrigins.js
@@ -1,7 +1,6 @@
 const allowedOrigins = [,
     'http://localhost:3000',
-    'https://the-network-xre0.onrender.com',
-    
+    'https://the-network-xre0.onrender.com/*',    
 ];
 
 module.exports = allowedOrigins;
